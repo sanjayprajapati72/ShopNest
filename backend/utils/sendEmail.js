d21a3187b513084e3c2e1bed9c9bf2eeaf4a2169
sanjayprajapati72 <sanjayprajapati72617@gmail.com>
2026-08-01 21:11:46 +0530

    bug fix email

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -56,20 +56,30 @@
 
 // module.exports = sendEmail;
 
-
 const nodemailer = require("nodemailer");
+
+console.log("EMAIL USER:", process.env.EMAIL_USER);
+console.log("EMAIL PASS:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
+
 
 const sendEmail = async ({ email, subject, message }) => {
   try {
+
     const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: Number(process.env.EMAIL_PORT),
+      host: "smtp-relay.brevo.com",
+      port: 587,
       secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
     });
+
+
+    // Check SMTP connection
+    await transporter.verify();
+    console.log("✅ SMTP Server Connected");
+
 
     const info = await transporter.sendMail({
       from: process.env.EMAIL_FROM,
@@ -78,14 +88,17 @@ const sendEmail = async ({ email, subject, message }) => {
       html: message,
     });
 
+
     console.log("✅ Email sent successfully");
-    console.log(info.messageId);
+    console.log("Message ID:", info.messageId);
 
     return info;
+
   } catch (error) {
-    console.error("❌ Email Error:", error);
+    console.error("❌ Email Error:", error.message);
     throw error;
   }
 };
+
 
 module.exports = sendEmail;
